784d334d3ea68440a3294cfa910cd3bba88056f6
16096003 <16096003@L815WD220017.edu.cbe.ab.ca>
2022-09-12 13:55:11 -0600

    1:55pm

--- Chapter 2/src/TicTacToe.java
+++ Chapter 2/src/TicTacToe.java
@@ -1,4 +1,18 @@
-
+import java.util.Scanner;
 public class TicTacToe {
-
+	  public static void main(String[] args) {
+		    Scanner myObj = new Scanner(System.in);
+		    System.out.println("     |     | ");
+		    System.out.println("     |     | ");
+		    System.out.println("     |     | ");
+		    System.out.println("-----|-----|-----");
+		    System.out.println("     |     | ");
+		    System.out.println("     |  x  | ");
+		    System.out.println("     |     | ");
+		    System.out.println("-----|-----|-----");
+		    System.out.println("     |     | ");
+		    System.out.println("     |     | ");
+		    System.out.println("     |     | ");
+		    		
+}
 }
--- Chapter 2/src/Yourinitialasanoutput.java
+++ Chapter 2/src/Yourinitialasanoutput.java
@@ -1,4 +1,13 @@
-
+import java.util.Scanner;
 public class Yourinitialasanoutput {
+	
 
-}
+	
+	  public static void main(String[] args) {//Don't mark this didn't have to book so I made it 
+	    Scanner myObj = new Scanner(System.in);
+	    System.out.println("");
+
+	
+	  }
+	}
+
