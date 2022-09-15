105594c6c82dec93fef48d6d9aebbfd04a7565d2
16096003 <16096003@L815WD220022.edu.cbe.ab.ca>
2022-09-15 15:16:54 -0600

    3:16

--- Chapter 3/src/SkillBuilding/Digits.java
+++ Chapter 3/src/SkillBuilding/Digits.java
@@ -3,13 +3,20 @@ import java.util.Scanner;
 public class Digits {
 
 	public static void main(String[] args) {
-		int onesplace;
-		int tensplace;
+		Scanner myObj = new Scanner(System.in);
+		int onesplace=0;
+		int tensplace=0;
 		// TODO Auto-generated method stub
-		System.out.println("Please eneter a two digit number ");
-		 Scanner myObj = new Scanner(System.in);
-		 onesplace=myObj.nextInt();
-		 System.out.println(onesplace );
+		
+		System.out.println("Please eneter a two digit number "); 
+		 int Number = myObj.nextInt();
+		 
+		 tensplace = Number / 10;
+		 System.out.println("tens place is" +tensplace );
+		 
+		 
+		 onesplace= Number%10        ;
+		 System.out.println("ones place is" +onesplace );
 	}
 
 }
